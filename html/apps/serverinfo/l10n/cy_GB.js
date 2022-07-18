59b7b130e3b8dc796318d4182c538cd76e6254de
gessildojunior <gessildojunior@gmail.com>
2022-07-18 16:13:13 -0300

    Segundo Commit

---
+++ html/apps/serverinfo/l10n/cy_GB.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "serverinfo",
+    {
+    "Users" : "Defnyddwyr",
+    "Groups" : "Grwpiau",
+    "Copy" : "Copïo",
+    "Size:" : "Maint:",
+    "Type:" : "Math:",
+    "Size" : "Maint",
+    "Files" : "Ffeiliau",
+    "Version" : "Fersiwn",
+    "Type" : "Math"
+},
+"nplurals=4; plural=(n==1) ? 0 : (n==2) ? 1 : (n != 8 && n != 11) ? 2 : 3;");
---
+++ html/config/config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocgiijyfenir',
+  'passwordsalt' => 'LLfFW87bzdVRsC3kprxgcI4qrfeUiM',
+  'secret' => '8Y5lTswgX8HMVCM2jAun6KxFScnpLjqwMtoJhdEvISe4Z4uD',
+  'trusted_domains' => 
+  array (
+    0 => '150.161.234.81:81',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '24.0.2.1',
+  'overwrite.cli.url' => 'http://150.161.234.81:81',
+  'installed' => true,
+);
